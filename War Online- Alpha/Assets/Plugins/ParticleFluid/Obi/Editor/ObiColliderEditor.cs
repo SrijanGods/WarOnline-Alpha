40efa5110947ea225cd8663e242bea5bacdab6c1
SrijanGods <srijangod10@gmail.com>
2020-05-20 16:12:44 +0530

    Initial Upload 2
    Part 2

---
+++ War Online- Alpha/Assets/Plugins/ParticleFluid/Obi/Editor/ObiColliderEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Obi{
+	
+	/**
+	 * Custom inspector for ObiCollider assets. 
+	 */
+	
+	[CustomEditor(typeof(ObiCollider)), CanEditMultipleObjects] 
+	public class ObiColliderEditor : Editor
+	{
+		
+		ObiCollider collider;	
+		
+		public void OnEnable(){
+			collider = (ObiCollider)target;
+		}
+		
+		public override void OnInspectorGUI() {
+			
+			serializedObject.UpdateIfRequiredOrScript();
+
+			Editor.DrawPropertiesExcluding(serializedObject,"m_Script");
+
+			// Apply changes to the serializedProperty
+			if (GUI.changed){
+				serializedObject.ApplyModifiedProperties();
+			}
+			
+		}
+		
+	}
+}
+
+
---
+++ War Online- Alpha/Assets/Plugins/ParticleFluid/Obi/Scripts/Utils/ObiKinematicVelocities.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ObiKinematicVelocities : MonoBehaviour {
+
+	private Quaternion prevRotation;
+	private Vector3 prevPosition;
+	private Rigidbody rigidbody;
+
+	void Awake(){
+		rigidbody = GetComponent<Rigidbody>();
+		prevPosition = transform.position;
+		prevRotation = transform.rotation;
+	}
+
+	void LateUpdate(){
+
+		if (rigidbody.isKinematic)
+		{
+			// differentiate positions to obtain linear velocity:
+			rigidbody.velocity = (transform.position - prevPosition) / Time.deltaTime;
+
+			// differentiate rotations to obtain angular velocity:
+			Quaternion delta = transform.rotation * Quaternion.Inverse(prevRotation);
+			rigidbody.angularVelocity = new Vector3(delta.x,delta.y,delta.z) * 2.0f / Time.deltaTime;
+		}
+
+		prevPosition = transform.position;
+		prevRotation = transform.rotation;
+
+	}
+}
