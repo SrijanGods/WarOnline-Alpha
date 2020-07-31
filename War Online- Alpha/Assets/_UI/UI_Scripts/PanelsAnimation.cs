5b8c043f22e6c45ddb089adf0b660650d3171d23
sarathak1 <sarthak2.tiwad@gmail.com>
2020-07-31 13:44:53 +0530

    UI update

---
+++ War Online- Alpha/Assets/_UI/UI_Scripts/PanelsAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsAnimation : MonoBehaviour
+{
+
+    public void DisplayTrue()
+    {
+        GetComponent<Animator>().SetBool("display", true);
+    }
+    public void DisplayFalse()
+    {
+        GetComponent<Animator>().SetBool("display", false);
+    }
+
+
+
+}
