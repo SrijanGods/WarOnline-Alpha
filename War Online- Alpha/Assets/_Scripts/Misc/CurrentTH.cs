e54557d07bb8099ff872804b1cfece94e5cae60c
sarathak1 <sarthak2.tiwad@gmail.com>
2020-08-13 19:52:36 +0530

    ui fix part 3

---
+++ War Online- Alpha/Assets/_Scripts/Misc/CurrentTH.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentTH : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI currentHullName;
+    [SerializeField] TextMeshProUGUI currentTurrentName;
+    [SerializeField] Image hullImage;
+    [SerializeField] Image turrentImage;
+    [SerializeField] Sprite[] turrentImages;
+    [SerializeField] Sprite[] hullImages;
+    [SerializeField] string hullName;
+    [SerializeField] string turrentName;
+
+    private void Start()
+    {
+        hullName = GlobalValues.Instance.hull;
+        turrentName = GlobalValues.Instance.turret;
+    }
+    private void Update()
+    {
+        SetTHText();
+        SetTurrentImage();
+        SetHullImage();
+    }
+
+    private void SetTHText()
+    {
+        currentHullName.text = hullName;
+        currentTurrentName.text = turrentName;
+    }
+
+    private void SetTurrentImage()
+    {
+        foreach (Sprite sprite in turrentImages)
+        {
+            if (sprite.name == turrentName)
+            {
+
+                turrentImage.sprite = sprite;
+            }
+
+            else {  return; }
+            return;
+        }
+        return;
+    }
+
+    private void SetHullImage()
+    {
+        foreach (Sprite sprite in hullImages)
+        {
+            if (sprite.name == hullName)
+            {
+                hullImage.sprite = sprite;
+            }
+
+            else
+            {
+                return;
+            }
+            return;
+        }
+        return;
+    }
+}
