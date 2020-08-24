e75987d9940359ee6458e0b79d03dc5fa336e874
Rahul Raman <rahulraman0108@gmail.com>
2020-08-24 19:24:27 +0530

    Fix: 2 scripts were not added to previous commit.

---
+++ War Online- Alpha/Assets/_Scripts/Controls/SimulatedInput.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+namespace _Scripts.Controls
+{
+    public class SimulatedInput : UserInputs.ITankActions
+    {
+        #region Variables & Init
+
+        private static readonly Dictionary<InputCodes, float> AxesValues = new Dictionary<InputCodes, float>();
+
+        private static readonly Dictionary<InputCodes, SimButtonControl> BtnValues =
+            new Dictionary<InputCodes, SimButtonControl>();
+
+        private static readonly Dictionary<InputCodes, bool> Simulated = new Dictionary<InputCodes, bool>();
+
+        private static SimulatedInput _instance;
+
+        private SimulatedInput()
+        {
+            var inputs = new UserInputs();
+
+            inputs.Enable();
+
+            inputs.Tank.Enable();
+            inputs.Tank.SetCallbacks(this);
+        }
+
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void OnBeforeSceneLoadRuntimeMethod()
+        {
+            _instance = new SimulatedInput();
+        }
+
+        #endregion
+
+        #region Set Values
+
+        public static void SetAxis(InputCodes name, float value, bool simulated)
+        {
+            if (Simulated.ContainsKey(name))
+            {
+                if (Simulated[name] && !simulated) return;
+                if (!Simulated[name] && simulated) return;
+            }
+
+            AxesValues[name] = value;
+        }
+
+        public static void SetButton(InputCodes name, SimButtonControl bc, bool simulated)
+        {
+            if (Simulated.ContainsKey(name))
+            {
+                if (Simulated[name] && !simulated) return;
+                if (!Simulated[name] && simulated) return;
+            }
+
+            BtnValues[name] = bc;
+        }
+
+        #endregion
+
+        #region API Calls
+
+        public static float GetAxis(InputCodes name)
+        {
+            if (AxesValues.ContainsKey(name))
+                return AxesValues[name];
+
+            return 0;
+        }
+
+        public static bool GetButtonDown(InputCodes name)
+        {
+            return BtnValues.ContainsKey(name) && BtnValues[name].down;
+        }
+
+        public static bool GetButton(InputCodes name)
+        {
+            return BtnValues.ContainsKey(name) && BtnValues[name].pressed;
+        }
+
+        public static bool GetButtonUp(InputCodes name)
+        {
+            return BtnValues.ContainsKey(name) && BtnValues[name].up;
+        }
+
+        public static void SimulateInput(InputCodes name, bool value)
+        {
+            Simulated[name] = value;
+        }
+
+        #endregion
+
+        #region Tank Input Callbacks
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            var v = context.ReadValue<Vector2>();
+
+            SetAxis(InputCodes.TankMoveX, v.x, false);
+            SetAxis(InputCodes.TankMoveY, v.y, false);
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            var v = context.ReadValue<Vector2>();
+
+            SetAxis(InputCodes.MouseLookX, v.x, false);
+            SetAxis(InputCodes.MouseLookY, v.y, false);
+        }
+
+        public void OnRecenter(InputAction.CallbackContext context)
+        {
+            if (!(context.control is ButtonControl btn)) return;
+
+            var s = new SimButtonControl
+            {
+                down = btn.wasPressedThisFrame, pressed = btn.isPressed, up = btn.wasReleasedThisFrame
+            };
+
+            SetButton(InputCodes.Recenter, s, false);
+        }
+
+        public void OnFire(InputAction.CallbackContext context)
+        {
+            if (!(context.control is ButtonControl btn)) return;
+
+            var s = new SimButtonControl
+            {
+                down = btn.wasPressedThisFrame, pressed = btn.isPressed, up = btn.wasReleasedThisFrame
+            };
+
+            SetButton(InputCodes.TankFire, s, false);
+        }
+
+        #endregion
+    }
+
+    [Serializable]
+    public enum InputCodes
+    {
+        MouseLookX,
+        MouseLookY,
+        TankMoveX,
+        TankMoveY,
+        Recenter,
+        TankFire
+    }
+
+    [Serializable]
+    public struct SimButtonControl
+    {
+        public bool down, pressed, up;
+    }
+}
---
+++ War Online- Alpha/Assets/_Scripts/Tank/New/TankCustomInputs.cs
@@ -0,0 +1,31 @@
+﻿using _Scripts.Controls;
+using UnityEngine;
+
+namespace _Scripts.Tank.New
+{
+    [RequireComponent(typeof(RTC_TankController))]
+    public class TankCustomInputs : MonoBehaviour
+    {
+        #region Variables & Init
+
+        private RTC_TankController _tankController;
+
+        private void Awake()
+        {
+            _tankController = GetComponent<RTC_TankController>();
+        }
+
+        #endregion
+
+        #region Updating
+
+        private void FixedUpdate()
+        {
+            _tankController.gasInput = SimulatedInput.GetAxis(InputCodes.TankMoveY);
+            _tankController.steerInput = SimulatedInput.GetAxis(InputCodes.TankMoveX);
+            _tankController.brakeInput = -Mathf.Clamp(SimulatedInput.GetAxis(InputCodes.TankMoveY), -1f, 0f);
+        }
+
+        #endregion
+    }
+}
