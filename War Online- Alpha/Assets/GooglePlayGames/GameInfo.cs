93881f00edae53129374608051786cc5dd468966
SrijanGods <srijangod10@gmail.com>
2020-09-23 18:35:48 +0530

    Removed SG Patcher, added Google Sign In SDK
    SG Patcher giving probs, deleted it.

--- War Online- Alpha/Assets/GooglePlayGames/GameInfo.cs
+++ War Online- Alpha/Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "969519935631"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "969519935631-gvg2afp7jr3brkecoe9gmntcvgvngi49.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
--- War Online- Alpha/Assets/SIDGIN/SIDGIN.Patcher/Readme.txt
+++
@@ -1,4 +0,0 @@
-Documetation: http://docs.sidgin.com/books/sg-patcher
-Youtube: https://www.youtube.com/playlist?list=PLhlDSEuHvfiv9rjUVOlB94Mgom0675wsr
-Support: unitysupport@sidgin.com
-Discord: https://discord.com/invite/KcyQ3gc
