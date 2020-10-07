602450bbc2b51ac41d6e5991c8b1294885358354
SrijanGods <srijangod10@gmail.com>
2020-10-07 17:14:06 +0530

    SG Patcher & Tank Prefabs Optimisation
    Updated SG Patcher with latest version.
    Optimised Tank Prefabs.

---
+++ War Online- Alpha/Assets/SIDGIN/SIDGIN.Patcher/Editor/SGPatcherWindow.cs
@@ -0,0 +1,15 @@
+﻿
+using SIDGIN.Patcher.Editors;
+using UnityEditor;
+
+namespace SIDGIN.Patcher.Unity.Editors
+{
+    public static class SGPatcherWindow
+    {
+        [MenuItem("Tools/SG Patcher")]
+        public static void ShowWindow()
+        {
+            MainWindow.ShowWindow();
+        }
+    }
+}
